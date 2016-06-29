43f8b7e8e27a32123f96a2ef14df442d63c76cd8
Rebecca Snyder <snyderrc@gmail.com>
2016-06-29 13:04:49 -0700

    added custom fields

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -13,37 +13,49 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 
-<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); 
 
-	$services = get_field('services');
-	$client = get_field('client');
-	$link = get_field('site_link');
-	$image_1 = get_field('image_1');
-	$image_2 = get_field('image_2');
-	$image_3 = get_field('image_3');
+		$services = get_field('services');
+		$client = get_field('client');
+		$link = get_field('site_link');
+		$image_1 = get_field('image_1');
+		$image_2 = get_field('image_2');
+		$image_3 = get_field('image_3'); 
+		$size = "full"; ?>
 
-<?php endwhile; // end of the loop. ?>
+	
 
 <article class="case-study">
-<aside class="case-study-sidebar">
-	<h2>MailChimp Campaign</h2>
-	<p id="gray">Content Strategy, Design & Development</p>
-	<p>Client: MailChimp</p>
-	<p> This is a project I  made for this company for this. Here is the early stage startup that I helped design and do front-end and back-end dev for.</p>
-	<h4>Visit Live Site</h4>
-	<?php the_content(); ?>
-</aside>
+	<aside class="case-study-sidebar">
+		<h2><?php the_title(); ?></h2>
+		<p id="gray"><?php echo $services; ?></p>
+		<p>Client: <?php echo $client; ?></p>
+
+		<?php the_content(); ?>
+
+		<p><a href="<?php echo $link; ?>">Site Link</a></p>	
+	</aside>
+
+	<div class="case-study-images">
+		<?php if (image_1) { ?>
+		<img src="<?php echo wp_get_attachment_image( $image_1, $size); ?> 
+			<?php } ?>
+
+		<?php if (image_2) { ?>
+		<img src="<?php echo wp_get_attachment_image( $image_2, $size); ?> 
+			<?php } ?>
+
+		<?php if (image_3) { ?>
+		<img src="<?php echo wp_get_attachment_image( $image_3, $size); ?> 
+			<?php } ?>
 
 
+	</div>
+</article>
 
+	<?php endwhile; // end of the loop. ?>
 
-
-<div class="samples">
-
-
-</div>
-
-		</div><!-- #content -->
+	</div><!-- #content -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
