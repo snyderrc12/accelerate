9bb406ce15b8373cd06c714018b6ea4b6d33c2f1
Rebecca Snyder <snyderrc@gmail.com>
2016-07-03 11:52:05 -0700

    styled homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -19,7 +19,7 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
+				<a class="button" href="<?php echo home_url(); ?>">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
 	</div><!-- .site-content -->
@@ -29,39 +29,42 @@ get_header(); ?>
 	<div class="site-content">
 		<h4>Featured Work</h4>
 
+		<ul class="homepage-featured-work">
 		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
 
 			<?php while ( have_posts() ) : the_post(); 
 				$image_1 = get_field("image_1");
-				size = "medium";
+				$size = "medium";
 			?>
+
+			<li class="individual-featured-work">
 				<figure>
 					<?php echo wp_get_attachment_image($image_1, $size); ?>
 				</figure>
 
 				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</li>
 				 
  
 			<?php endwhile; // end of the loop. ?>
 
 			<?php wp_reset_query(); // resets the altered query back to the original ?>
-
-
+		</ul>
 
 	</div>
 </section>
-
-
-
-
 
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
 			<h4>From the Blog</h4>
-
-			
-
+			<?php query_posts('posts_per_page=1'); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<?php the_excerpt(); ?>
+				<a href="<?php the_permalink(); ?>" class="read-more-link">Read More</a>
+			<?php endwhile; // end of the loop. ?>
+			<?php wp_reset_query(); // resets the altered query back to the original ?>
 		</div> 
 	</div>
 </section>
